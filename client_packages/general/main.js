0240f4b0368e2bd80a59cee4ff833a1543932c85
TendoDE <104503996+TendoDE@users.noreply.github.com>
2024-02-19 23:23:03 +0100

    change hud color and clientside changes

--- client_packages/general/main.js
+++ client_packages/general/main.js
@@ -1,1 +1,6 @@
+"use strict";
 mp.discord.update('Spielt auf HC-RP.de [ALPHA]', 'Coming soon...');
+// Update Hud Color to darkgreen
+mp.events.add('testClientEvent', () => {
+    mp.game.ui.setHudColour(166, 57, 102, 57, 255);
+});
